9347bd28b45517ec9bf23f26e7bfab1f2b31acb8
HavallonTech <kestercode@gmail.com>
2023-10-23 23:33:39 +0100

    rxcrpyion

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
+
 def safe_print_list(my_list=[], x=0):
-	count = 0
-	for b in my_list:
-		try:
-			print("{}".format(my_list[b]))
-			count = count + 1
-		except(IndexError):
-			print("Index Error")
-		finally:
-			print("")
-	 return (count)
+
+    count = 0
+
+    for b in range(x):
+        try:
+            print("{}".format(my_list[b]), end="")
+            count = count + 1
+        except IndexError:
+            print("Index Error")
+        finally:
+            print("")
+    return (count)
