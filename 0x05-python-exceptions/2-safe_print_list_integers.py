3ce46e4eb863bc6b6ce082f9e1e797f4b2e4f245
HavallonTech <kestercode@gmail.com>
2023-10-24 19:56:02 +0100

    exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    intcount = 0
+    intcount = x
+    size = 0
+
+    for length in my_list:
+        size = size + 1
     for a in range(0, x):
         try:
             if type(my_list[a]) is int:
