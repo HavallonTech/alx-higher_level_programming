669ea1339ff198f80b755c94fc7d26dc57f01fa6
HavallonTech <kestercode@gmail.com>
2023-10-24 15:43:22 +0100

    exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,11 @@
 
 def safe_print_division(a, b):
     try:
-        division = a / b
+        if (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            division = a / b
     except ZeroDivisionError:
         division = None
     finally:
         print("Inside result: {}".format(division))
-        #print("Inside result:",formatted_value)
+        """print("Inside result:",formatted_value)"""
         return (division)
