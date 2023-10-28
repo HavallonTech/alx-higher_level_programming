bad32c15a57c1060ccb2ef43c3fc265b071dec1b
HavallonTech <kestercode@gmail.com>
2023-10-28 21:01:13 +0100

    slass alax

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A definintion of an empty class """
+
+
+class Square:
+    """A pass statement used to walk throug the class methods"""
+
+    def __init__(self, size):
+        self.size = size
