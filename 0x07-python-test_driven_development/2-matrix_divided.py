1fd54f2dd9074a16bb6c4dfba1f0c4f3c4f68b01
HavallonTech <kestercode@gmail.com>
2023-11-01 06:46:34 -0700

    ALX

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+A module contains a function that divides each element of a matriX
+"""
+
+
+def matrix_divided(matrix, div):
+    """"
+    function that divides all elements of a matrix.
+
+    Args:
+        matrix: list of list of ints or/and floats
+        div: the element that divides the matrix
+
+    Raises:
+        TypeError:
+            must be list of lists
+            all elements must be an int or float
+    Returns:
+        the divided matrix
+    """
+    new_matrix = []
+    if (type(div) is not int) and (type(div) is not float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if type(matrix) is not list:
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
+    if len(matrix) == 0:
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
+    for i in matrix:
+        row_len = len(matrix[0])
+        if type(i) is not list:
+            raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+                    )
+        if len(i) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for j in i:
+            if (type(j) is not int) and (type(j) is not float):
+                raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
+            res = j / div
+            new_ele = float("{:.2f}".format(res))
+            new_row.append(new_ele)
+
+        new_matrix.append(new_row)
+    return (new_matrix)
---
+++ 0x07-python-test_driven_development/REAME.md
@@ -0,0 +1,28 @@
+0x07. Python - Test-driven development
+Python
+UnitTests
+TDD
+ By: Guillaume
+ Weight: 1
+ Ongoing second chance project - started Oct 26, 2023 6:00 AM, must end by Nov 4, 2023 6:00 AM
+ An auto review will be launched at the deadline
+In a nutshell…
+Auto QA review: 0.0/167 mandatory & 0.0/104 optional
+Altogether:  0.0%
+Mandatory: 0.0%
+Optional: 0.0%
+Calculation:  0.0% + (0.0% * 0.0%)  == 0.0%
+Concepts
+For this project, we expect you to look at this concept:
+
+Never forget a test
+
+
+Background Context
+Important notice on intranet checks for Python projects
+Starting from today:
+
+Based on the requirements of each task, you should always write the documentation (module(s) + function(s)) and tests first, before you actually code anything
+The intranet checks for Python projects won’t be released before their first deadline, in order for you to focus more on TDD and think about all possible cases
+We strongly encourage you to work together on test cases, so that you don’t miss any edge case. But not in the implementation of them!
+Don’t trust the user, always think about all possible edge cases
