a4d9f634bb2678ab3b0f3cccc1c2c5c32dda56e7
HavallonTech <kestercode@gmail.com>
2023-12-05 20:35:42 +0100

    Inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,6 +8,6 @@ def is_same_class(obj, a_class):
     """A function that returns true if the exert same
      object is passed
     """
-    myvalue = type(obj) == a_class
+    myvalue = isinstance(obj, a_class)
     if myvalue:
         return (myvalue)
