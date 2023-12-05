8c594fee388afecd8dbe52a522d4e5a3579f98c9
HavallonTech <kestercode@gmail.com>
2023-12-05 20:26:05 +0100

    Inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module Exarct same object
+"""
+
+
+def is_same_class(obj, a_class):
+    """A function that returns true if the exert same
+     object is passed
+    """
+    myvalue = type(obj) == a_class
+    if myvalue:
+        return (myvalue)
