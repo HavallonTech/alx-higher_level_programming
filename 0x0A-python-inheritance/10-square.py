f54ebb581be6b45f0ca34232d2733962d90bec1f
HavallonTech <kestercode@gmail.com>
2023-12-05 23:58:18 +0100

    inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle (9-rectangle.py)"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ class square """
+    def __init__(self, size):
+        """ constructor for instantiation of size """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = sizei
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle (9-rectangle.py)"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ class Square """
+
+    def __init__(self, size):
+        """
+        Constructor __init__ - A constructor for instantiation.
+        """
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """
+        Calculate the area of the square.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        String representation of the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
