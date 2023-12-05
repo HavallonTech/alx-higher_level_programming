62c63c7a8c44fb4c611ae990385814e5f28ede5c
HavallonTech <kestercode@gmail.com>
2023-12-05 18:49:44 +0100

    pythong inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
-	
     """
         Returns a list of attributes and methods of the given object.
-
-        Parameters:
-       - obj: The object to inspect.
-
-    Returns:
-    - List of attributes and methods.
     """
     return dir(obj)
