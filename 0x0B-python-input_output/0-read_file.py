c97a031359a2cb8ff284b8328e73b45aa5b7fb6a
HavallonTech <kestercode@gmail.com>
2023-12-06 23:14:33 +0100

    files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module for 0-read_file.py
+"""
+
+
+def read_file(filename=""):
+    """
+      The function to read from a file
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except Exception as e:
+        print(f"Error: {e}")
