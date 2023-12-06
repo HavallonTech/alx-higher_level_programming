9b8b843f3cb468275dd36e501a8a1a01b5f87f7a
HavallonTech <kestercode@gmail.com>
2023-12-06 23:34:20 +0100

    file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
 Module for 0-read_file.py
+function that reads a text file (UTF8) and prints it to stdout:
 """
 
 
 def read_file(filename=""):
-    """
-      The function to read from a file
-    """
+    """ The function to read from a file """
     with open(filename, 'r', encoding='utf-8') as ff:
         file_content = ff.read()
         print(file_content, end = "")
