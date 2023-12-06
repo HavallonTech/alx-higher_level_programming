634d9e07cece599fecc76627849ee9ed415102e2
HavallonTech <kestercode@gmail.com>
2023-12-06 23:30:12 +0100

    files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     """
     with open(filename, 'r', encoding='utf-8') as ff:
         file_content = ff.read()
-        print(file_content, end = ' ')
+        print(file_content, end = "")
