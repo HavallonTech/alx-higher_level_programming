68535a0cb0a93a6d7d022f01482ef1b9f324094c
HavallonTech <kestercode@gmail.com>
2023-12-08 21:48:39 +0100

    files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,10 +8,9 @@ and then save them to a file:
 
 import os
 import json
-form sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = \
-    __import__('6-load_from_json_file.py').load_from_json_file
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     existing_data_item = load_from_json_file("add_item.json")
     combined_data = existing_data_item + argv[1:]
