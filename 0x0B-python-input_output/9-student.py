8bd59d65d34690078a5e313b7f8489ce6af19d5e
HavallonTech <kestercode@gmail.com>
2023-12-08 22:13:03 +0100

    file input

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for 9-student.py
+Write a class Student that defines a student by:
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+    """
+      default method for all instance
+    """
+        Student.first_name = first_name
+        Student.last_name = last_name
+        Student.age = age
+
+    def to_json(self):
+        return self.__dict__
