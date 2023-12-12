1ee55f70add83803201bec8230f83b18c6fc283e
HavallonTech <kestercode@gmail.com>
2023-12-12 10:40:50 +0100

    ok

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,10 +9,17 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Init method always called when an instance is raise
+        """
         if (id is not None):
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    """
     def draw(list_rectangles, list_squares):
+        """
+         the draw method
+        """
         pass
+    """
