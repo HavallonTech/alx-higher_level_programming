e50f597d97be109be3aff4d35a80d876e6c05388
HavallonTech <kestercode@gmail.com>
2023-12-13 04:53:54 +0100

    almost one circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,10 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    Square class inheriting  from the Rectangle class
+        Square class inheriting  from the Rectangle class
+        Args: 
+            Rectangle: Theis class inherits from the Rectangle
+            class
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
@@ -14,14 +17,19 @@ class Square(Rectangle):
     @property
     def size(self):
         """
-        Retrieve the size of the rectangle
+            Retrieve the size of the rectangle
+            Return:
+                Returns the setted value for the size
+                whivh is leght and breadth
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-        Set the value of the width
+            Set the value of the width
+            Args:
+                value which is the size of the square
         """
         self.width = value
         self.height = value
@@ -30,7 +38,9 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """
-        return the dictionary representaion of the Square
+            return the dictionary representaion of the Square
+            Return: Returns the dictionary representation of
+            the square
         """
         dictionary = {
                 "id": self.id,
@@ -42,7 +52,9 @@ class Square(Rectangle):
 
     def __str__(self):
         """
-        str method
+            str method
+            return: 
+                dormatted dictionary
         """
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y, self.width)
