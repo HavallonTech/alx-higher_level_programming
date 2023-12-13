601d88ebdfe3844968282069ecdc0b29bddf859c
HavallonTech <kestercode@gmail.com>
2023-12-13 04:12:25 +0100

    almost one circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,9 +11,17 @@ class Base:
 
     def __init__(self, id=None):
         """Init method always called when an instance is raise
+        Args:
+            id (int, optional): Assignef to the id attribute.
+            otherwise, increments __nb_objects and assigns the new value to id.
         """
         if (id is not None):
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def draw():
+        """draw function of the base class
+        """
+        pass
