532403cb357ed668d81340f5b6e93f4b38ad4345
HavallonTech <kestercode@gmail.com>
2024-01-17 15:45:21 +0100

    ALX SQL

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- Write a script that prints the full description of the table first_table from
 -- the database hbtn_0c_0
 -- The database name will be passed as an argument of the mysql command
--- You are not allowed to use the DESCRIBE or EXPLAIN statements
-DESCRIBE first_table;
+-- You are not allowed to use the DESCRIBE or EXPLAIN 
+SHOW CREATE TABLE first_table;
