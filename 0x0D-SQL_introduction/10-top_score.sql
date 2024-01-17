99cec719c6e1bf35554363c5a2b4112756eae751
HavallonTech <kestercode@gmail.com>
2024-01-17 17:10:57 +0100

    ALX SQL

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
--- a script that lists all records of the table second_table of the database order DESC
+-- a script that lists all records of the table second_table of the database. ORD Desc
 SELECT score, name
 FROM second_table
 ORDER BY score DESC;
