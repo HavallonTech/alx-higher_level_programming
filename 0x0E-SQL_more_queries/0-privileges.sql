9b9572aebcf40d9c6033634e58ab2efeb36ccc1a
HavallonTech <kestercode@gmail.com>
2024-01-18 15:37:52 +0100

    SQL Querieies

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,19 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 
 -- on your server (in localhost).
-SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
-SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
+-- SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
+-- SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
+SET @user_exists = (SELECT EXISTS(SELECT 1 FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost'));
+
+IF @user_exists THEN
+    SHOW GRANTS FOR 'user_0d_1'@'localhost';
+ELSE
+    SELECT 'User does not exist';
+END IF;
+SET @user_exists = (SELECT EXISTS(SELECT 1 FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost'));
+
+IF @user_exists THEN
+    SHOW GRANTS FOR 'user_0d_2'@'localhost';
+ELSE
+    SELECT 'User does not exist';
+END IF;
+
