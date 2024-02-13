eab8ce97ecbebb73b0a17e736ecdabeb88098d0c
HavallonTech <kestercode@gmail.com>
2024-02-13 10:45:37 +0100

    Javascript

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*script that prints a the first argument passed to it*/
+/*	script that prints a the first argument passed to it */
 
 const argv = process.argv[2];
 
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-/* Write a script that prints My number: <first argument converted in integer>
-if the first argument can be converted to an integer*/
+/* Write a script that prints My number: <first argument converted in integer> */
+/* if the first argument can be converted to an integer */
 
-let argument = process.argv[2];
+const argument = process.argv[2];
 const intValue = parseInt(argument);
 if (intValue > 0 ) {
-	//const intValue = parseInt(argument);
-	let final = 'My number: ' + intValue;
+	const final = 'My number: ' + intValue;
 	console.log(final);
 }
 else{
-console.log('Not a number');
+	console.log('Not a number');
 }
