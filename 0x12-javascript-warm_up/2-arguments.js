8a37b5c6635fe4468f88d94ea0585df1159cb15c
HavallonTech <kestercode@gmail.com>
2024-02-13 03:07:55 +0100

    Javascript warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 /* W script that prints a message depending of the number of arguments passed */
 
-const argv = arguments.length;
+/*const argv = arguments.length; */
+
+const argv = process.argv.length;
 if (argv < 1){
 console.log('No argument');
 }
