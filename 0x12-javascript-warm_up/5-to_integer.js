451099b093cf483fd3bcca7614dbca12935f3bb8
HavallonTech <kestercode@gmail.com>
2024-02-13 04:16:02 +0100

    Javascript

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Write a script that prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer*/
+
+let argument = process.argv[2];
+if (typeof argument !== 'undefined') {
+	const intValue = parseInt(argument);
+	let final = 'My number: ' + intValue;
+	console.log(final);
+}
+else{
+console.log('Not a number')
+}
