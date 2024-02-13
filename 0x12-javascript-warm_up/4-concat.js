86aff5aea470b928b7d429fb51d2a58744631f95
HavallonTech <kestercode@gmail.com>
2024-02-13 10:41:24 +0100

    Javascript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*script that concat two argument passed*/
+/*	script that concat two argument passed */
 
-let final = process.argv[2] + ' is ' + process.argv[3];
+const final = process.argv[2] + ' is ' + process.argv[3];
 console.log(final);
