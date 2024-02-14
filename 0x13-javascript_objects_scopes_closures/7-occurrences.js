1c7a766400fb9b2dd087e437f05519db0238caa1
HavallonTech <kestercode@gmail.com>
2024-02-14 04:21:23 +0100

    Javascript Object

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,31 +2,16 @@
 
 /** class Rectangle that defines a square: */
 
-const square = require('./5-square');
 
-class Square extends square {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    let container = '';
-    if (c !== undefined) {
-      for (let i = 0; i < this.width; i++) {
-        container += c;
-      }
-      for (let j = 0; j < this.height; j++) {
-        console.log(container);
-      }
-    } else {
-      for (let k = 0; k < this.width; k++) {
-        container += 'X';
-      }
-      for (let l = 0; l < this.height; l++) {
-        console.log(container);
-      }
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  let counter = 0;
+  while (i < list.length) {
+    if (list[i] === searchElement) {
+      counter = counter + 1;
     }
+    i++;
   }
-}
 
-module.exports = Square;
+  return counter;
+};
