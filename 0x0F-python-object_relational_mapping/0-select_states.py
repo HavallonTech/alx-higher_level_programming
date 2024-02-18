b0c8e293ade63c74d136264fb8cb76cfbe6f3c92
HavallonTech <kestercode@gmail.com>
2024-02-18 03:38:36 +0100

    SQL select in ORM

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa:
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        port=3306,
+        db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * from states ORDERED BY id ASC")
+    myrow = cur.fetchall()
+    for row in myrow:
+        for col in row:
+            print("%s," % col)
+        print("\n")
+    cur.close()
+    db.close()
