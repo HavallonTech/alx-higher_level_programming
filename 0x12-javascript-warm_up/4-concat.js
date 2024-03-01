1eaa95605f519a4d83cc09e962e533cc58046fd8
kester <132357927+HavallonTech@users.noreply.github.com>
2024-03-01 22:55:39 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-/*	script that concat two argument passed */
-
-const final = process.argv[2] + ' is ' + process.argv[3];
-console.log(final);
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/* Write  script that prints the addition of 2 integers */
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
-function add(a, b){
-	let sum = a + b;
-	console.log(sum);
-}
-add(a,b);
