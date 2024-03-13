dc62aa174f124bec5e2f4815d7fdcf23cd5822d4
HavallonTech <kestercode@gmail.com>
2024-03-13 07:25:28 -0700

    Javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+
+const process = require('process');
+
+// Printing process.argv property value
+let args = process.argv;
+
+let totalargs = args.length;
+if (totalargs <=  2){
+        console.log('No argument');
+}
+else if (totalargs ===  3){
+        console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
