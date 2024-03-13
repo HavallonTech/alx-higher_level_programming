81b1c864ca60c6fcd8dab6421f5f5913508b660b
HavallonTech <kestercode@gmail.com>
2024-03-13 23:32:08 +0100

    Javascript

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-
-const process = require('process');
-
 let argsv = process.argv.slice(2);
+
 if(argsv[0] === undefined){
-console.log('No argument');
+	console.log('No argument');
 }
 else {
-console.log(argsv[0]);
+	console.log(argsv[0]);
 }
