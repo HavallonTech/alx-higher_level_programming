f27ad1cd599f36c4c0b1dda09f4964ef5d465154
HavallonTech <kestercode@gmail.com>
2024-03-14 00:40:10 +0100

    Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const cars = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const array_of_element = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (i of cars){
-console.log(i);
+for (const element of array_of_element) {
+  console.log(element);
 }
