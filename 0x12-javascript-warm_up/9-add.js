2ed0db4cbee861d30f6384b417041346a239259f
HavallonTech <kestercode@gmail.com>
2024-03-14 01:50:16 +0100

    Javascript

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+
+const args = process.argv.slice(2);
+
+
+if (parseInt(args[0])  && parseInt(args[1])){
+	sum = parseInt(args[0]) + parseInt(args[1]);
+	console.log(sum);
+}
+else
+{
+	sum = parseInt(args[0]) + parseInt(args[1]);
+	console.log(sum);
+}
