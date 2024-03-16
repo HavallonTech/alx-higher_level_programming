fe22e34c56f69a00787ec32968dbb370ad2a47e3
HavallonTech <kestercode@gmail.com>
2024-03-16 11:58:35 +0100

    Javascriptit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,8 +11,8 @@ else if (args[1] === undefined)
 	console.log(0);
 }
 else{
-	args.sort(a, b => a - b);
-	//const newArray = Array.from(new Set(args)).sort((a, b) => b - a);
-	//console.log(newArray[1]);
-	console.log(args[1]);
+	//args.sort(a, b => a - b);
+	const newArray = Array.from(new Set(args)).sort((a, b) => b - a);
+	console.log(newArray[1]);
+	//console.log(args[1]);
 }
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 exports.add = function add(num1, num2){
 	return(parseInt(num1) + parseInt(num2));
 } 
