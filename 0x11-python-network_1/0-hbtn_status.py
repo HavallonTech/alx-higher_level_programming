3c707cc145bf80a5a7a4f66f39be1bcc08ead42d
HavallonTech <kestercode@gmail.com>
2024-03-28 17:26:51 -0700

    Network 01

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import urllib.request
+
+target_url ='https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(target_url) as response:
+    data = response.read()
+    decoded_data = data.decode('utf-8')
+    #print(decoded_data)
+    #print(data)
+    print(f"Body response:\n\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    #   print(f"\t- utf8 content: {the_page.decode('utf-8')}")
+    print(f"\t- utf8 content: {decoded_data}")
