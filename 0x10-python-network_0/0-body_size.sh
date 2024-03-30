00defbdbd1d51a671714c25486d227ad173fe1ea
HavallonTech <kestercode@gmail.com>
2024-03-29 18:27:23 -0700

    ALX

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-#The size must be displayed in bytes
-#You have to use curl
-#Please test your script in the sandbox provided, using the web server running on port 5000
 
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
