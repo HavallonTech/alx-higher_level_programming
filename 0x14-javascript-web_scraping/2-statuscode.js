6ed42446eb475cca226cecb38f96612910345abb
HavallonTech <kestercode@gmail.com>
2024-04-24 00:35:27 +0100

    Javascript

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,25 @@
 #!/usr/bin/node
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
 const request = require('request');
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.error(err);
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.error(error);
   } else {
-    const result = JSON.parse(body);
-    console.log(result.title);
+    console.log(`code: ${response.statusCode}`);
+  }
+});#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
   }
 });
